e0c6a4f6f97e7035ada2597651d8adfce1b6e84d
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-14 11:26:02 +0300

    commit tree 2ade829f7a8afc7690a53c453061044ca82ebe0c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let oneLine = document.getElementById("text");
+    oneLine.textContent = "This is really cool!"
+  });
+  
+  
